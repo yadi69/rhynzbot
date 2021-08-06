8f52b20c8b8f403fd236966c130c6059a26d679e
RhynzID <42452952+RhynzID@users.noreply.github.com>
2021-08-06 10:08:33 +0700

    Update igstory.js

--- plugins/igstory.js
+++ plugins/igstory.js
@@ -3,7 +3,7 @@ const { igstory } = require('../lib/scrape')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
   if (!args[0]) throw `*Perintah ini untuk mengunduh story instagram*\n\ncontoh:\n\n${usedPrefix + command} stikerinbot`
-  if (args[0].startsWith('http') || args[0].startsWith('@')) throw `*Username salah! Perintah ini untuk mengunduh story instagram*\n\ncontoh:\n${usedPrefix + command} stikerinbot`
+  if (args[0].startsWith('http') || args[0].startsWith('@')) throw `*Username salah! Perintah ini untuk mengunduh story instagram*\n\ncontoh:\n${usedPrefix + command} rhynz`
 
   igstory(args[0]).then(async res => {
     let igs = JSON.stringify(res)
