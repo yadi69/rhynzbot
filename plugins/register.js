d2287ea18057be1218869b796c1b580ce110e538
RhynzID <42452952+RhynzID@users.noreply.github.com>
2021-08-11 09:14:45 +0700

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -1,54 +1,34 @@
 const { createHash } = require('crypto')
-let Reg = /\|?(.*)([.|])([0-9]*)$/i
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix }) {
-  
-  let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
   let user = global.db.data.users[m.sender]
-  let tnbot = (await conn.getFile(await conn.getProfilePicture(m.fromMe))).data.toString('base64')
-  if (user.registered === true) throw `Anda sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
-  if (!Reg.test(text)) throw `Format salah\n\n*${usedPrefix}daftar <nama>|<umur>*`
+  if (user.registered === true) throw `Kamu sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
+  if (!Reg.test(text)) throw `Format salah\n*${usedPrefix}daftar nama.umur*`
   let [_, name, splitter, age] = text.match(Reg)
   if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
   if (!age) throw 'Umur tidak boleh kosong (Angka)'
-  if (age < 18) throw 'Maaf, Anda belum bisa mendaftar.'
-  if (age > 40) throw 'Maaf, Anda terlalu tua.'
-  user.name = name
-  user.age = parseInt(age)
+  age = parseInt(age)
+  if (age > 70) throw 'Umur terlalu tua'
+  if (age < 5) throw 'Bayi bisa ngetik sesuai format bjir ._.'
+  user.name = name.trim()
+  user.age = age
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
-  let caption = `
-┏ ┅ ━━━━━━━━━━━━━━━━━━━━━ ┅ ━
-┇       *「 INFORMATION 」*
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━━ ┅ ━
-┃
-┃ *Nama:* ${name}
-┃ *Umur:* ${age} tahun
-┃ *Gift:* Rp10000
-┃ *Registered:* ${rtotalreg}
-┃ *Serial Number:* 
-┃ ${sn}
-┗ ┅ ━━━━━━━━━━━━━━━━━━━━━ ┅ ━
-
- _Simpan Serial Number anda!_
-`.trim()
-await conn.reply(m.chat, caption,/* { 
-  key: { 
-    remoteJid: 'status@broadcast', 
-    participant: '0@s.whatsapp.net', 
-    fromMe: false 
-  }, message: { 
-    "imageMessage": { 
-      "mimetype": "image/jpeg", 
-      "caption": `Registration Successful!`, 
-      "jpegThumbnail": tnbot} } }, { contextInfo: { mentionedJid: [m.sender] } }*/m)
-global.db.data.users[m.sender].uang += 10000
+  m.reply(`
+Daftar berhasil!
+┌─〔 Info 〕
+├ Nama: ${name}
+├ Umur: ${age} tahun
+├ Gift: Rp10000
+├ Registered: ${rtotalreg}
+├ SN: ${sn}
+└────
+`.trim())
 }
-handler.help = ['daftar', 'register'].map(v => v + ' <nama>|<umur>')
-handler.tags = ['main']
+handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <nama>.<umur>')
+handler.tags = ['xp']
 
 handler.command = /^(daftar|reg(ister)?)$/i
-handler.disabled = false
 
 module.exports = handler
-
