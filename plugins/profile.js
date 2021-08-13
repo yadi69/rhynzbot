033190ae48b92374f1cb3ce69962327d9c7c95dc
RhynzID <42452952+RhynzID@users.noreply.github.com>
2021-08-13 19:38:06 +0700

    Add files via upload

--- plugins/profile.js
+++ plugins/profile.js
@@ -1,52 +1,26 @@
-let PhoneNumber = require('awesome-phonenumber')
-let levelling = require('../lib/levelling')
 let handler = async (m, { conn, usedPrefix }) => {
   let pp = './src/avatar_contact.png'
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   try {
-    pp = await conn.getProfilePicture(who)
+    pp = await conn.getProfilePicture(m.sender)
   } catch (e) {
 
   } finally {
-    let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
-    if (typeof global.db.data.users[who] == "undefined") {
-      global.db.data.users[who] = {
-        exp: 0,
-        limit: 10,
-        lastclaim: 0,
-        registered: false,
-        name: conn.getName(m.sender),
-        age: -1,
-        regTime: -1,
-        afk: -1,
-        afkReason: '',
-        banned: false,
-        level: 0,
-        call: 0,
-        role: 'Bronze',
-        autolevelup: false,
-        pc: 0,
-      }
-    }
-    let { name, limit, exp, lastclaim, registered, regTime, age, level, role, banned } = global.db.data.users[who]
-    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
-    let username = conn.getName(who)
-    let math = max - xp
+    let name = m.fromMe ? conn.user : conn.contacts[m.sender]
     let str = `
-Nama: ${username} ${registered ? '(' + name + ') ' : ''}(@${who.replace(/@.+/, '')})${about != 401 ? '\nInfo: ' + about : ''}
-Nomor: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-Link: https://wa.me/${who.split`@`[0]}${registered ? '\nUmur: ' + age : ''}
-XP: TOTAL ${exp} (${exp - min} / ${xp}) [${math <= 0 ? `Siap untuk *${usedPrefix}levelup*` : `${math} XP lagi untuk levelup`}]
-Level: ${level}
-Role: *${role}*
-Limit: ${limit}
-Terdaftar: ${registered ? 'Ya (' + new Date(regTime).toLocaleString() + ')' : 'Tidak'}${lastclaim > 0 ? '\nTerakhir Klaim: ' + new Date(lastclaim).toLocaleString() : ''}
+Name: ${name.vnmae || name.notify || name.name || ('+' + name.jid.split`@`[0])} (@${m.sender.replace(/@.+/, '')})
+Number: +${m.sender.split`@`[0]}
+Link: https://wa.me/${m.sender.split`@`[0]}
+${readMore}
+\n\n*Mau nge check isi inventorymu? Ketik ${usedPrefix}inv*
 `.trim()
-    let mentionedJid = [who]
-    conn.sendFile(m.chat, pp, 'pp.jpg', banned ? str : 'banned', m, false, { contextInfo: { mentionedJid } })
+    let mentionedJid = [m.sender]
+    conn.sendFile(m.chat, pp, 'pp.jpg', str, m, false, { contextInfo: { mentionedJid }})
   }
 }
-handler.help = ['profile [@user]']
-handler.tags = ['tools']
-handler.command = /^profile?$/i
+handler.help = ['profile']
+handler.tags = ['rpg']
+handler.command = /^(profile|profil|pp|propile|propil)$/i
 module.exports = handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
