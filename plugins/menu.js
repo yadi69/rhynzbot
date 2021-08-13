c708042040022874c5234cc8295312ddc9f33671
RhynzID <42452952+RhynzID@users.noreply.github.com>
2021-08-13 18:14:01 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -166,7 +166,7 @@ handler.owner = false
 handler.mods = false
 handler.premium = false
 handler.group = false
-handler.register = true
+handler.register = false
 handler.private = false
 
 handler.admin = false
