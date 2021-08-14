46142c4b9be784605e1d8fe68f772182c20b22e0
RhynzID <42452952+RhynzID@users.noreply.github.com>
2021-08-15 06:17:56 +0700

    Update wallpaper.js

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
         let res = await fetch(global.API('xteam', '/randomimage/wpmobile', {}, 'APIKEY'))
         let img = await res.buffer()
         conn.sendMessage(m.chat, img, MessageType.image, {
-            quoted: m, caption: '*©ヴァイオレット*'
+            quoted: m, caption: '*©rhynz'
         })
     } catch (e) {
         console.log(e)
