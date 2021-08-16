ddc53eb44e2c66cab026c0e281b57cbbf9a78699
RhynzID <42452952+RhynzID@users.noreply.github.com>
2021-08-16 10:26:54 +0700

    Update tekateki_ans.js

--- plugins/tekateki_ans.js
+++ plugins/tekateki_ans.js
@@ -6,15 +6,18 @@ handler.before = async function (m) {
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*tete/i.test(m.quoted.text)) return !0
     this.tekateki = this.tekateki ? this.tekateki : {}
     if (!(id in this.tekateki)) return m.reply('Soal itu telah berakhir')
-    if (m.quoted.id == this.tekateki[id][0].id) {
-        let json = JSON.parse(JSON.stringify(this.tekateki[id][1]))
-        if (/^.*tete$/i.test(m.text)) return !0
-        if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.tekateki[id][2]
-            m.reply(`*Benar!*\n+${this.tekateki[id][2]} XP`)
-            clearTimeout(this.tekateki[id][3])
-            delete this.tekateki[id]
-        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
+    if (/^nyerah$/i.test(m.text)) {
+        await this.sendButton(m.chat, `Jawabannya adalah ${JSON.parse(JSON.stringify(this.tebaklirik[id][1].jawaban))}`.trim(), '© rhynz', 'TEBAK LIRIK', '.tebaklirik').then(() => {
+            delete this.tebaklirik[id]
+            throw 0
+        })
+    }
+    // if (m.quoted.id == this.tebaklirik[id][0].id) {
+    let json = JSON.parse(JSON.stringify(this.tebaklirik[id][1]))
+    if (['.teli', 'BANTUAN', ''].includes(m.text)) return !0
+    if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
+        global.db.data.users[m.sender].exp += this.tebaklirik[id][2]
+        await this.sendButton(m.chat, `*Benar!* +${this.tebaklirik[id][2]} XP`.trim(), '© rhynz', 'TEBAK LIRIK', '.tebaklirik')
         else m.reply(`*Salah!*`)
     }
     return !0
