63b1b7d80c98f2b66a01809d8c13286da803f467
RhynzID <42452952+RhynzID@users.noreply.github.com>
2021-08-16 10:16:09 +0700

    Update and rename tebakgambar_answer.js to tebakgambar_ans.js

--- plugins/tebakgambar_ans.js
+++ plugins/tebakgambar_ans.js
@@ -5,13 +5,15 @@ handler.before = async function (m) {
   let id = m.chat
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*hint/i.test(m.quoted.contentText)) return !0
   this.tebakgambar = this.tebakgambar ? this.tebakgambar : {}
-  if (!(id in this.tebakgambar)) return m.reply('Soal itu telah berakhir')
+  if (/^nyerah$/i.test(m.text)) {
+    await this.sendButton(m.chat, `Jawabannya adalah ${JSON.parse(JSON.stringify(this.tebakgambar[id][1].jawaban))}`.trim(), '© rhynz', 'TEBAK GAMBAR', '.tebakgambar').then(() => { delete this.tebakgambar[id] })
+  }
   // if (m.quoted.id == this.tebakgambar[id][0].id) { // undefined T_T
   let json = JSON.parse(JSON.stringify(this.tebakgambar[id][1]))
-  if (/^.*hint$/i.test(m.text)) return !0
+  if (['.hint', 'BANTUAN', ''].includes(m.text)) return !0
   if (m.text.toLowerCase() == json.jawaban.toLowerCase()) {
     global.db.data.users[m.sender].exp += this.tebakgambar[id][2]
-    m.reply(`*Benar!*\n+${this.tebakgambar[id][2]} XP`)
+    await this.sendButton(m.chat, `*Benar!* +${this.tebakgambar[id][2]} XP`, '© rhynz', 'TEBAK GAMBAR', '.tebakgambar')
     clearTimeout(this.tebakgambar[id][3])
     delete this.tebakgambar[id]
   } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
