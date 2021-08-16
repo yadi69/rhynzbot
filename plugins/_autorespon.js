01d3ca4a1a1e4b782c1a7e606b2d67cded77d6f7
RhynzID <42452952+RhynzID@users.noreply.github.com>
2021-08-16 21:35:08 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -4,28 +4,32 @@ handler.all = async function (m, { isBlocked }) {
     if (isBlocked) return
     if (m.isBaileys) return
     if (m.chat.endsWith('broadcast')) return
-    let setting = global.db.data.settings
-    let { isBanned } = global.db.data.chats[m.chat]
+    let setting = db.data.settings
+    let { isBanned } = db.data.chats[m.chat]
+    let { banned } = db.data.users[m.sender]
+
     // ketika ditag
     try {
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
-            await this.send2Button(m.chat, `uhm.. iya ada apa?${m.msg.contextInfo.expiration == 604800 ? '\n\nmatiin pesan sementaranya, biar tombolnya bisa dipake' : ''}`.trim(), '© RHYNZ | pesan otomatis', `${isBanned ? 'UNBAN' : 'MENU'}`, `${isBanned ? '.unban' : '.?'}`, `${!m.isGroup ? 'DONASI' : isBanned ? 'UNBAN' : 'BAN'}`, `${!m.isGroup ? '.donasi' : isBanned ? '.unban' : '.ban'}`)
+            await this.send2Button(m.chat,
+                isBanned ? 'bot tidak aktif' : banned ? 'kamu dibanned' : 'bot disini',
+                '© rhynz',
+                isBanned ? 'UNBAN' : banned ? 'PEMILIK BOT' : 'MENU',
+                isBanned ? '.unban' : banned ? '.owner' : '.?',
+                m.isGroup ? 'BAN' : isBanned ? 'UNBAN' : 'DONASI',
+                m.isGroup ? '.ban' : isBanned ? '.unban' : '.donasi')
         }
     } catch (e) {
         return
-    }
+	@@ -21,11 +28,8 @@ handler.all = async function (m, { isBlocked }) {
     // ketika ada yang invite/kirim link grup di chat pribadi
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
-        this.reply(m.chat, `┌〔 Undang Bot ke Grup 〕
-│ 
-├ 7 Hari / Rp 10.000
-├ 30 Hari / Rp 25.000
-│ 
-├ Hubungi @${global.owner[0]}
+        this.sendButton(m.chat, `┌〔 Undang Bot ke Grup 〕
+├ 7 Hari / Rp 5,000
+├ 30 Hari / Rp 10,000
 └────
-`.trim(), m, { contextInfo: { mentionedJid: [global.owner[0] + '@s.whatsapp.net'] } })
+`.trim(), '© rhynz', 'PEMILIK BOT', ',owner', { contextInfo: { mentionedJid: [global.owner[0] + '@s.whatsapp.net'] } })
     }
-
     // salam
     let reg = /(ass?alam|اَلسَّلاَمُ عَلَيْكُمْ|السلام عليکم)/i
     let isSalam = reg.exec(m.text)
@@ -35,15 +39,7 @@ handler.all = async function (m, { isBlocked }) {
     // backup db
     if (setting.backup) {
         if (new Date() * 1 - setting.backupDB > 1000 * 60 * 60) {
-            let d = new Date
-            let date = d.toLocaleDateString('id', {
-                day: 'numeric',
-                month: 'long',
-                year: 'numeric'
-            })
-            await global.db.write()
-            this.reply(global.owner[0] + '@s.whatsapp.net', `Database: ${date}`, null)
-            this.sendFile(global.owner[0] + '@s.whatsapp.net', fs.readFileSync('./database.json'), 'database.json', '', 0, 0, { mimetype: 'application/json' })
+	@@ -53,26 +52,21 @@ handler.all = async function (m, { isBlocked }) {
             setting.backupDB = new Date() * 1
         }
     }
@@ -51,7 +47,7 @@ handler.all = async function (m, { isBlocked }) {
     if (new Date() * 1 - setting.status > 1000) {
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
-        await this.setStatus(`Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} | hanya kang recode rhynz`).catch(_ => _)
+        await this.setStatus(`Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} | rhynz`).catch(_ => _)
         setting.status = new Date() * 1
     }
 }
