3549294b1feadc41026f3c75e137aee39b3f9383
RhynzID <42452952+RhynzID@users.noreply.github.com>
2021-08-16 09:17:12 +0700

    Update igstory.js

--- plugins/igstory.js
+++ plugins/igstory.js
@@ -2,14 +2,14 @@ const { igstory } = require('../lib/scrape')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-  if (!args[0]) throw `*Perintah ini untuk mengunduh story instagram*\n\ncontoh:\n\n${usedPrefix + command} stikerinbot`
-  if (args[0].startsWith('http') || args[0].startsWith('@')) throw `*Username salah! Perintah ini untuk mengunduh story instagram*\n\ncontoh:\n${usedPrefix + command} rhynz`
-
+if (!args[0]) throw `uhm.. username nya mana?\n\ncontoh:\n\n${usedPrefix + command} rhynz`
+if (args[0].startsWith('http') || args[0].startsWith('@')) throw `username salah`
   igstory(args[0]).then(async res => {
     let igs = JSON.stringify(res)
     let json = JSON.parse(igs)
     for (let { downloadUrl, type } of json)
-      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '*© rhynz*', m, { thumbnail: Buffer.alloc(0) })
+      await m.reply(global.wait)
+     conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '© rhynz', m)
 
   })
 
