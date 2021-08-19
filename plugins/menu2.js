8fe6031d156ece50e1f5bf4e126a9ff1da0c867b
RhynzID <42452952+RhynzID@users.noreply.github.com>
2021-08-20 04:38:13 +0700

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -385,14 +385,15 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
     throw e
   }
 }
-handler.help = ['menu', 'help', '?']
+handler.help = ['menu2', 'help2', '?']
 handler.tags = ['main']
-handler.command = /^(menu|help|\?)$/i
+handler.command = /^(menu2|help2|\?)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
 handler.group = false
 handler.private = false
+handler.register = true
 
 handler.admin = false
 handler.botAdmin = false
