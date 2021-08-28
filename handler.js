c5ebc0dbcc4dec5d876294a58b66ce5adad2a116
RhynzID <42452952+RhynzID@users.noreply.github.com>
2021-08-28 17:36:29 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -265,7 +265,6 @@ module.exports = {
         let plugin = global.plugins[name]
         if (!plugin) continue
         if (plugin.disabled) continue
-        if (!opts['restrict']) if (plugin.tags && plugin.tags.includes('admin')) continue
         const str2Regex = str => str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
         let _prefix = plugin.customPrefix ? plugin.customPrefix : conn.prefix ? conn.prefix : global.prefix
         let match = (_prefix instanceof RegExp ? // RegExp Mode?
